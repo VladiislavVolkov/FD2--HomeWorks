0aaf8b3a3b87bfc201e856a2788ac3d198a7fad0
VladiislavVolkov <kali.laskaa@gmail.com>
2023-01-22 22:30:20 +0300

    Hometask#2. 3 of 3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Треугольник в цикле
+// Треугольник 
 
 for (let i = '#'; i.length <= 7; i += '#') {
     console.log(i);
@@ -8,14 +8,33 @@ for (let i = '#'; i.length <= 7; i += '#') {
 
 // Шахматная доска
 
-let i = "";
+let i = '';
 for (let a = 0; a < 8; a++) {
   for (let b = 0; b < 8; b++) {
     if ((a + b) % 2 !== 0)
-      i += " ";
+      i += ' ';
     else
-      i += "#";
+      i += '#';
   }
-  i += "\n";
+  i += '\n';
 }
 console.log(i);
+
+
+
+// Число 100
+
+let number
+do {
+  number = prompt('Введите число и узнаете угадали ли Вы его!');
+  if (number === null) {
+    alert('Жаль, что покидаете нас ...')
+    break
+  }
+  if (number > 100) {
+    alert('Ура, Вы угадали!')
+  }
+  if (number <= 100) {
+    alert('Попробуйте еще раз. У Вас получится!')
+  }
+} while (number <= 100)
